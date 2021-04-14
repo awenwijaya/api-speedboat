afd5820ac987b90d8f97495886257267f8341d41
awenwijaya <61014772+awenwijaya@users.noreply.github.com>
2021-04-14 13:53:53 +0800

    first commit

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::post('lihat-pelabuhan', 'App\Http\Controllers\ApiController@lihat_pelabuhan');
+Route::post('lihat-kapal', 'App\Http\Controllers\ApiController@lihat_kapal');
+Route::get('lihat-kapal-univ', 'App\Http\Controllers\ApiController@lihat_kapal_univ');
+Route::post('lihat-jadwal', 'App\Http\Controllers\ApiController@lihat_jadwal');
+Route::post('daftar-chat-id', 'App\Http\Controllers\ApiController@daftar_chat_id');
